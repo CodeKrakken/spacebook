54a34bd7dc630844412abf8aba97b0e14d577afb
Ed Courage <edcourage@live.com>
2019-12-12 15:01:16 +0000

    more logic added

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-# RSpec.describe Comment, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Comment, type: :model do
+
+  context '#update_valid?' do
+  let (:user) { create(:user) }
+  let (:post) { create(:post) }
+    it "update is still valid if posted within the last 10 mins" do
+
+      comment = user.comments.create(message: "Can you update me", post_id: post.id)
+      expect(comment.update_valid?).to be_truthy
+    end
+  end
+end
